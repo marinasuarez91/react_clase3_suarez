573f1d5e51d41f00369b298d0dc8b7b3294287e1
marinasuarez <m.suarez.seguin@gmail.com>
2022-01-12 18:56:27 -0300

    clase3

---
+++ my-app/src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import "./NavBar.css"
+
+function NavBar () {
+    return <nav className="NavStyle">
+        <div className="Brand">.nina</div>
+        <ul className ="liStyles">
+            <li>Home</li>
+            <li>Tienda</li>
+            <li>FAQ</li>
+            <li>Contacto</li>
+        </ul>
+    </nav>
+}
+
+export default NavBar
